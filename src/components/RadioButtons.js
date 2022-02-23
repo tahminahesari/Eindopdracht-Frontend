970794d4483d1a08141cf536a1727c899eb2dbca
tahminahesari <hesari@live.com>
2022-02-23 17:28:41 +0100

    Game:  useState en controlled component aangemaakt

--- src/components/RadioButtons.js
+++ src/components/RadioButtons.js
@@ -2,55 +2,120 @@ import React from "react";
 import "../components/RadioButtons.css";
 
 export default function RadioButtons(props) {
+  console.log(props);
+  function chooseMember(member) {
+    props.setSelectedMember(member);
+    props.setQuestionsTried(props.questionsTried + 1);
+    if (props.correctMember === member) {
+      props.setScore(props.score + 1);
+    }
+  }
+
+  const isAMemberSelected = props.selectedMember !== "";
   return (
     <div className="radio-buttons">
       <div className="radio-content">
-        <input id="radio1" type="radio" name="radio" />
-        <label for="radio1">
+        <input
+          id="radio1"
+          type="radio"
+          name="radio"
+          checked={props.selectedMember === "Jin"}
+          onChange={() => {
+            props.setSelectedMember("Jin");
+            props.setQuestionsTried(props.questionsTried + 1);
+            if (props.correctMember === "Jin") {
+              props.setScore(props.score + 1);
+            }
+          }}
+          disabled={props.selectedMember !== ""}
+        />
+        <label htmlFor="radio1">
           <span></span> Jin
         </label>
       </div>
       <br />
       <div className="radio-content">
-        <input id="radio2" type="radio" name="radio" />
-        <label for="radio2">
+        <input
+          id="radio2"
+          type="radio"
+          name="radio"
+          checked={props.selectedMember === "Suga"}
+          onChange={() => chooseMember("Suga")}
+          disabled={isAMemberSelected}
+        />
+        <label htmlFor="radio2">
           <span></span> Suga
         </label>
       </div>
       <br />
       <div className="radio-content">
-        <input id="radio3" type="radio" name="radio" />
-        <label for="radio3">
+        <input
+          id="radio3"
+          type="radio"
+          name="radio"
+          checked={props.selectedMember === "J-hope"}
+          onChange={() => chooseMember("J-hope")}
+          disabled={isAMemberSelected}
+        />
+        <label htmlFor="radio3">
           <span></span> J-hope
         </label>
       </div>
       <br />
       <div className="radio-content">
-        <input id="radio4" type="radio" name="radio" />
-        <label for="radio4">
+        <input
+          id="radio4"
+          type="radio"
+          name="radio"
+          checked={props.selectedMember === "RM"}
+          onChange={() => chooseMember("RM")}
+          disabled={isAMemberSelected}
+        />
+        <label htmlFor="radio4">
           <span></span> RM
         </label>
       </div>
       <br />
       <div className="radio-content">
-        <input id="radio5" type="radio" name="radio" />
-        <label for="radio5">
+        <input
+          id="radio5"
+          type="radio"
+          name="radio"
+          checked={props.selectedMember === "Jimin"}
+          onChange={() => chooseMember("Jimin")}
+          disabled={isAMemberSelected}
+        />
+        <label htmlFor="radio5">
           <span></span> Jimin
         </label>
       </div>
       <br />
       <div className="radio-content">
-        <input id="radio6" type="radio" name="radio" />
-        <label for="radio6">
+        <input
+          id="radio6"
+          type="radio"
+          name="radio"
+          checked={props.selectedMember === "V"}
+          onChange={() => chooseMember("V")}
+          disabled={isAMemberSelected}
+        />
+        <label htmlFor="radio6">
           <span></span> V
         </label>
       </div>
       <br />
 
       <div className="radio-content">
-        <input id="radio7" type="radio" name="radio" />
-        <label for="radio7">
-          <span></span> Jungkook
+        <input
+          id="radio7"
+          type="radio"
+          name="radio"
+          checked={props.selectedMember === "JK"}
+          onChange={() => chooseMember("JK")}
+          disabled={isAMemberSelected}
+        />
+        <label htmlFor="radio7">
+          <span></span> JK
         </label>
       </div>
       <br />
--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Game.css";
 import "../components/Background.js";
 import Background from "../components/Background.js";
@@ -8,13 +8,47 @@ import Quote from "../components/Quote";
 import PageTitle from "../components/PageTitle";
 
 export default function Game() {
+  const [score, setScore] = useState(5);
+  const [selectedMember, setSelectedMember] = useState("");
+  const [quote, setQuote] = useState(
+    "`Even if you’re not perfect you’re limited edition`"
+  );
+  const [correctMember, setCorrectMember] = useState("Jin");
+  const [questionsTried, setQuestionsTried] = useState(10);
+
+  let feedback;
+  if (selectedMember === correctMember) {
+    feedback = "CORRECT!";
+  } else if (selectedMember === "") {
+    feedback = "Please select a member.";
+  } else {
+    feedback = "Oops it was actually: " + correctMember;
+  }
+  let color = "hotpink";
+
   return (
     <Background background={btblack}>
       <div>
-        <Quote quote="Some random quote" member="???" />
+        <p className="score">
+          {score} / {questionsTried} points
+        </p>
+        <p className="feedback" style={{ color: color }}>
+          {feedback}
+        </p>
+        <p className="selectedMember">{selectedMember}</p>
+
+        <Quote quote={quote} member="???" />
         <PageTitle>Who said this? </PageTitle>
 
-        <RadioButtons />
+        <RadioButtons
+          selectedMember={selectedMember}
+          setSelectedMember={setSelectedMember}
+          score={score}
+          setScore={setScore}
+          questionsTried={questionsTried}
+          setQuestionsTried={setQuestionsTried}
+          correctMember={correctMember}
+        />
       </div>
     </Background>
   );
