146028d99ba6b16f275053c41a1578a9c4a47809
tahminahesari <hesari@live.com>
2022-03-04 09:32:00 +0100

    Homepagina api quots opgehaald

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,45 +8,30 @@ import TransparentCard from "../components/TransparentCard";
 import axios from "axios";
 
 export default function Home() {
-  const [randomQuotes, setRandomQuotes] = useState({});
+  const [randomQuote, setRandomQuote] = useState({});
   console.log("Anneyong!");
 
   useEffect(() => {
-    async function fetchRandomQuotes() {
+    async function fetchRandomQuote() {
       try {
         const result = await axios.get(
           "https://bts-quotes-api.herokuapp.com/quote/random"
         );
         console.log(result);
-        setRandomQuotes(result.data);
+        setRandomQuote(result.data);
       } catch (error) {}
     }
-    fetchRandomQuotes();
+    fetchRandomQuote();
   }, []);
-  console.log(randomQuotes);
+
+  console.log(randomQuote);
 
   return (
     <Background background={bts}>
       <TransparentCard>
         <PageTitle>Get a random quote </PageTitle>
-        <Quote
-          quote='"Do you know what purple means? Purple is last color of the rainbow, it means I will trust you and love you for a long time. Borahae"'
-          member="V"
-        />
-        <Quote
-          quote='"No one knows tomorrow, but today we are the dream we followed yesterday"'
-          member="Suga"
-        />
-        <Quote
-          quote='"I have come to love myself for who I am, for who I was, and for who I hope to become"'
-          member="RM"
-        />
-        {randomQuotes.map((quote) => {
-          console.log(quote);
-          return (
-            <Quote key={quote.id} quote={quote.quote} member={quote.member} />
-          );
-        })}
+
+        <Quote quote={randomQuote.quote} member={randomQuote.member} />
       </TransparentCard>
     </Background>
   );
