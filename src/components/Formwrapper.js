043eaa418782628d83660fbdebe22219932233c7
tahminahesari <hesari@live.com>
2022-03-11 17:54:23 +0100

    back-up terug van 9 maart

--- src/components/Formwrapper.js
+++ src/components/Formwrapper.js
@@ -2,15 +2,11 @@ import React from "react";
 import "./Formwrapper.css";
 
 export default function Formwrapper(props) {
-  console.log(props.handleSubmit, props.onSubmit);
+  console.log(props);
   return (
     <div id="formwrapper">
       <h2>{props.formName}</h2>
-      <form onSubmit={props.handleSubmit(props.onSubmit)}>
-        {props.children}
-      </form>
+      <form>{props.children}</form>
     </div>
   );
-}
-{
 }
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,87 +1,44 @@
 import React, { useState } from "react";
 import Formwrapper from "../components/Formwrapper";
 import Background from "../components/Background.js";
-import { useForm } from "react-hook-form";
+// import { useForm } from "react-hook-form";
 
 export default function Signup() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
+  // const {
+  //   register,
+  //   handleSubmit,
+  //   formState: { errors },
+  // } = useForm();
 
-  function onSubmit(data) {
-    console.log(data);
-  }
+  // function onSubmit(data) {
+  //   console.log(data);
+  // }
 
-  console.log("ERRORS", errors);
+  // console.log("ERRORS", errors);
 
   return (
-    <Background>
-      <Formwrapper
-        handleSubmit={handleSubmit}
-        onSubmit={onSubmit}
-        formName="Sign up"
-      >
-        <label htmlFor="details-name">
-          Name:
-          <input
-            type="text"
-            id="details-name"
-            placeholder="Name"
-            {...register("name"), {
-              required: "Name is required",
-              minLength: {
-              value: 3,
-              message: "Name must contain at least 3 characters",
-              },
-            }}
-          />
-          {errors.name && <p>{errors.name.message}</p>}
-        </label>
-        <br />
-        <br />
+    <p>KOEN!</p>
+    // <Background>
+    //   <Formwrapper
+    //     handleSubmit={handleSubmit}
+    //     onSubmit={onSubmit}
+    //     formName="Sign up"
+    //   >
+    //     <label htmlFor="details-name">
+    //       Name:
+    //       <input type="text" id="details-name" placeholder="Name" />
+    //       {errors.name && <p>{errors.name.message}</p>}
+    //     </label>
+    //     <br />
+    //     <br />
 
-        <label htmlFor="email-account">
-          Email:
-          <input
-            type="email"
-            id="email-account"
-            placeholder="Email"
-            {...register("email"), {
-              required: {
-                value 
-              }
-            
-            }}
+    //     <br />
+    //     <br />
 
-          />
-          <br />
-          <br />
-        </label>
-
-        <label htmlFor="password">
-          Password:
-          <input type="password" id="password" {...register("password")} />
-          <br />
-        </label>
-
-        <label htmlFor="confirmPassword">
-          Confirm Password:
-          <input
-            id="confirmPassword"
-            type="password"
-            {...register("confirmPassword")}
-          />
-        </label>
-
-        <br />
-        <br />
-
-        <button id="signUp" type="submit">
-          Sign Up
-        </button>
-      </Formwrapper>
-    </Background>
+    //     <button id="signUp" type="submit">
+    //       Sign Up
+    //     </button>
+    //   </Formwrapper>
+    // </Background>
   );
 }
