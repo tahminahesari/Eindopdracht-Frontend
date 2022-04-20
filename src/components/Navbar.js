871b2ff01d5cf692f90628b7cdbbeabf8c166ee3
tahminahesari <hesari@live.com>
2022-04-20 14:20:28 +0200

    responsive styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
@@ -7,37 +7,43 @@ import { useContext } from "react";
 export default function Navbar() {
   const auth = useContext(AuthContext);
   const isSignedIn = auth.user.accessToken !== null;
+  const [menuOpen, setMenuOpen] = useState(false);
+  const closeMenu = () => setMenuOpen(false);
 
   return (
     <nav id="nav-bar" role="navigation">
       <div id="menuToggle">
-        <input type="checkbox" />
+        <input
+          type="checkbox"
+          checked={menuOpen}
+          onChange={() => setMenuOpen(!menuOpen)}
+        />
 
         <span></span>
         <span></span>
         <span></span>
 
         <ul id="menu">
-          <Link to="/">
+          <Link to="/" onClick={closeMenu}>
             <li>Home</li>
           </Link>
           {!isSignedIn && (
-            <Link to="/signup">
+            <Link to="/signup" onClick={closeMenu}>
               <li>Sign Up</li>
             </Link>
           )}
           {!isSignedIn && (
-            <Link to="/signIn">
+            <Link to="/signIn" onClick={closeMenu}>
               <li>Sign In</li>
             </Link>
           )}
-          <Link to="/upload">
+          <Link to="/upload" onClick={closeMenu}>
             <li>Upload {!isSignedIn && "(please sign in first)"}</li>
           </Link>
-          <Link to="/quote">
+          <Link to="/quote" onClick={closeMenu}>
             <li>Quotes</li>
           </Link>
-          <Link to="/game">
+          <Link to="/game" onClick={closeMenu}>
             <li>Quote Game</li>
           </Link>
           {isSignedIn && (
